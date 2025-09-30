94e2a942b48296efa870d815bad47475fab6766c
shu915 <chukichi915@gmail.com>
2025-09-30 14:38:45 +0900

    CRのフィードバックを受け入れる

--- src/components/organisms/Pagination.tsx
+++ src/components/organisms/Pagination.tsx
@@ -3,8 +3,10 @@ import { Button } from "@/components/ui/button";
 export default function Pagination({ page, setPage, totalItems, perPage }: { page: number, setPage: (page: number) => void, totalItems: number, perPage: number }) {
   
   const totalPages = Math.ceil(totalItems / perPage);
-  return (
-    <div className="flex gap-2 justify-center">
+
+    if (totalItems > 1) {
+      return (
+        <div className="flex gap-2 justify-center">
       {page !== 1 && <Button onClick={() => setPage(1)} className="bg-gray-500">{1}</Button>}
       {page > 3 && <Button onClick={() => setPage(page - 2)} className="bg-gray-500">{`${page - 2}`}</Button>}
       {page > 2 && <Button onClick={() => setPage(page - 1)} className="bg-gray-500">{`${page - 1}`}</Button>}
@@ -13,5 +15,7 @@ export default function Pagination({ page, setPage, totalItems, perPage }: { pag
       {page < totalPages - 1 && <Button onClick={() => setPage(page + 2)} className="bg-gray-500">{`${page + 2}`}</Button>}
       {page !== totalPages && <Button onClick={() => setPage(totalPages)} className="bg-gray-500">{`${totalPages}`}</Button>}
     </div>
-  );
+      );
+    }
+  return null;
 }
