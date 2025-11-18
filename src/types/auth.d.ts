9e5309e7ac8ee4f349518e97415b94019ab2550c
shu915 <chukichi915@gmail.com>
2025-11-18 14:35:25 +0900

    認証を改良する

---
+++ src/types/auth.d.ts
@@ -0,0 +1,20 @@
+import "next-auth";
+import "next-auth/jwt";
+
+declare module "next-auth" {
+  interface Session {
+    user: {
+      name?: string | null;
+      email?: string | null;
+      provider?: "google" | "email" | undefined;
+      provider_subject?: string | null | undefined;
+    };
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    provider?: "google" | "email" | undefined;
+    provider_subject?: string | null | undefined;
+  }
+}
